eef5a5c359e1cf18bac60670836db5068b734f98
Susumu Obata <obata@mir-ai.co.jp>
2026-03-28 21:42:18 +0900

    feat: qos 1

--- _ide_helper.php
+++ _ide_helper.php
@@ -29837,11 +29837,6 @@ namespace  {
 }
 
 
-namespace Facades\Livewire\Features\SupportFileUploads {
-    /**
-     * @mixin \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl     */
-    class GenerateSignedUploadUrl extends \Livewire\Features\SupportFileUploads\GenerateSignedUploadUrl {}
-}
 
 
 
